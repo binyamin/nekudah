5bfc9a6661471ce440f68a987bcb148ac8f4b934
Binyamin Aron Green <binyamingreen@protonmail.com>
2021-08-23 15:52:46 -0400

    initial commit

---
+++ example/cli.mjs
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+import lib from './frodo/index.js';
+
+const cmd = process.argv.slice(2)[0];
+
+switch (cmd) {
+    case "build":
+        await lib.build();
+        break;
+    case "watch":
+        lib.watch();
+        break;
+    case "serve":
+        // lib.watch();
+        lib.serve();
+        break;
+    case undefined:
+        console.log("Please provide either `watch`, `serve`, or `build`.")
+        process.exit(0);
+        break;
+    default:
+        console.error(`Command "${cmd}" not recognized`);
+        process.exit(1);
+        // break;
+}
---
+++ index.js
@@ -0,0 +1,106 @@
+import path, { relative, resolve } from "node:path";
+
+import { watch as createWatcher } from "chokidar";
+import log from "signale-logger";
+
+import Engine from "./lib/Engine.js";
+import Server from "./lib/Server.js";
+import { copyDir, walk, copyFile, readJSON } from "./lib/common/fs.js";
+
+const dirs = {
+    html: resolve("src"),
+    assets: resolve("static"),
+    out: resolve("out"),
+}
+
+async function build() {
+    await copyDir(dirs.assets, dirs.out)
+
+    const templates = (await walk(dirs.html))
+    .map(f => {
+        return relative(dirs.html, f);
+    })
+    .filter(f => {
+        return !f.startsWith("_partials") && f !== "macros.html";
+    })
+
+    const env = new Engine({ src: dirs.html, out: dirs.out })
+    env.addGlobal("bookmarks", await readJSON("./bookmarks.json"))
+
+    for(const t of templates) {
+        env.renderToFile(t);
+    }
+    log.success("Build complete")
+}
+
+const ftree = [
+    {
+        filepath: "_partials/layout.html",
+        dependents: [
+            "index.html",
+            "links.html"
+        ]
+    },
+    {
+        filepath: "_partials/test.html",
+        dependents: [
+            "index.html"
+        ]
+    }
+];
+
+function watch() {
+    const watcher = createWatcher([dirs.html, dirs.assets]);
+    log.watch("Recursively watching src and static directories");
+
+    watcher.on("change", async (fpath, _fstats) => {
+        const [rootdir, ...rest] = relative(process.cwd(), fpath).split(path.sep)
+        const relpath = rest.join(path.sep);
+
+        if(rootdir === path.basename(dirs.assets)) {
+            const outpath = resolve(dirs.out, relpath);
+            await copyFile(fpath, outpath);
+            log.info("Copied asset", relpath);
+        } else if(rootdir === path.basename(dirs.html)) {
+            const env = new Engine({ src: dirs.html, out: dirs.out })
+            env.addGlobal("bookmarks", await readJSON("./bookmarks.json"))
+
+            const fnode = ftree.find(o => o.filepath === relpath);
+
+            if(!!fnode) {
+                // file is a partial
+                fnode.dependents.forEach(p => {
+                    // render the templates which extend it
+                    env.renderToFile(p);
+                })
+            } else {
+                // file is a template
+                env.renderToFile(relpath);
+            }
+        } else {
+            log.error(new Error("path is not watched by chokidar: " + fpath));
+        }
+    })
+
+    process.on("SIGINT", process.exit)
+
+    process.on("exit", () => {
+        log.success("Exiting...");
+        watcher.unwatch();
+        process.exit();
+    })
+}
+
+function serve() {
+    const s = new Server({ dir: dirs.out, port: 3000 })
+    s.start();
+
+    process.on("SIGINT", process.exit)
+
+    process.on("exit", () => {
+        log.success("Exiting...");
+        s.stop()
+    })
+}
+
+export default { serve, build, watch };
