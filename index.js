4132ee14fe94573ce273143f52645d35610c0d1a
Binyamin Aron Green <binyamingreen@protonmail.com>
2021-09-01 16:12:41 -0400

    oops, broken import

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ export { default as Engine } from "./lib/Engine.js";
 export { default as Server } from "./lib/Server.js";
 
 import fs from './lib/common/fs.js';
-import log from './lib/common/log.js';
 
 export const utils = {fs, log};
